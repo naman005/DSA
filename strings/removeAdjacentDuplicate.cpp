69c8fb492bb66be4fc95b42b012d697fdfab7741
naman005 <namanchaturvedi05@gmail.com>
2025-07-10 22:06:08 +0530

    Remove Adjacent Duplicate

---
+++ strings/removeAdjacentDuplicate.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string result = "";
+        for(char ch : s) {
+            if(!result.empty() && result.back() == ch) result.pop_back();
+            else result.push_back(ch);
+        }
+        return result;
+    }
+};
